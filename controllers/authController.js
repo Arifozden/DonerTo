5ce43174ec4a5a8f495976979ee2385c2aeff36b
arifozden <arifozden1@gmail.com>
2024-01-06 00:34:02 +0100

    validations added

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,16 +2,20 @@ const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const Category = require('../models/Category');
 const Product = require('../models/Product');
+const { validationResult } = require('express-validator');
 
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
     res.status(201).redirect('/login');
   } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      error,
-    });
+    const errors = validationResult (req);
+
+    for(let i=0; i<errors.array().length; i++){
+      req.flash('error', `${errors.array()[i].msg}`);
+    }
+
+    res.status(400).redirect('/register');
   }
 };
 
@@ -30,10 +34,12 @@ exports.loginUser = async (req, res) => {
         req.session.userID = user._id;
         res.status(200).redirect('/users/dashboard');
       } else {
-        res.status(401).send('Invalid password');
+        req.flash('error', 'Password is not correct');
+        res.status(400).redirect('/login');
       }
     } else {
-      res.status(404).send('User not found');
+      req.flash('error', 'Email is not correct');
+      res.status(400).redirect('/login');
     }
   } catch (error) {
     console.error(error);
